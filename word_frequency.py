318b46bb15121aa2b42683062c223a70bca37ac2
Kalen Martin <kalen.martin1@gmail.com>
2022-02-23 22:11:28 -0500

    95% complete, is functional

--- word_frequency.py
+++ word_frequency.py
@@ -1,3 +1,8 @@
+from ast import keyword
+from cgitb import text
+from hashlib import new
+
+
 STOP_WORDS = [
     'a', 'an', 'and', 'are', 'as', 'at', 'be', 'by', 'for', 'from', 'has', 'he',
     'i', 'in', 'is', 'it', 'its', 'of', 'on', 'that', 'the', 'to', 'were',
@@ -7,9 +12,11 @@ STOP_WORDS = [
 # PUNCTUATION = !()-[]{};:'"\,<>./?@#$%^&*_~
 
 def print_word_freq(file):
-    """Read in `file` and print out the frequency of words in that file."""
+    d = dict()
+    
+    # """Read in `file` and print out the frequency of words in that file."""
     with open(file) as file:
-        d = dict()
+        # sd = sorted_dict()
         text_string = file.read()
         # print(text_string[0:100])
         print(f"{len(text_string)}")
@@ -27,28 +34,55 @@ def print_word_freq(file):
         text_string = text_string.replace("-", "")
         text_string = text_string.replace("—", "")
         text_string = text_string.replace('"', "")
-        text_string = text_string.lower().split()
+        text_string = text_string.lower()
+        newword_list = text_string.split()
         # print(text_string[0:100])
         # return text_string[0:100]
+
+        newword_list_filtered = {}
+        for word in newword_list:
+            if word not in STOP_WORDS:
+                if word in newword_list_filtered:
+                    newword_list_filtered[word] += 1
+                else:
+                    newword_list_filtered[word] = 1
     
-        text = text_string
-        stop_word_set = set(STOP_WORDS)
-        text_string = [item for item in text if item not in stop_word_set]
-        print(text_string[0:100])
+        newword_list_sorted = dict(sorted(newword_list_filtered.items(), key=lambda seq: seq[1], reverse=True))
+        
+        for key in newword_list_sorted:
+            print(f"{key:>20} | {newword_list_sorted[key]:2} {'*' * newword_list_sorted[key]}")
+        
+        # word_dict = {}
+        # for word in newword_list_sorted:
+        #     word_dict[word] = newword_list_sorted.count(word)
+        # for word in word_dict:
+        #     print(f"{word} | {word_dict.get(word)} {'*' * int(word_dict.get(word))}")
+        
+        
+        
+        # text = text_string
+        # stop_word_str = str(STOP_WORDS)
+        # text_string = [item for item in text if item not in stop_word_str]
+        # print(text_string[0:25])
         # return text_string [0:100]
 
-        for word in text_string:
-            # print(word)
-            if word in d:
-                d[word] = d[word] + 1
-            else:
-                d[word] = 1
+        # for word in text_string:
+        #     # print(word)
+        #     if word in d:
+        #         d[f"{word}"] += 1
+        #     else:
+        #         d[f"{word}"] = 1
                 
-        for key in list(d.keys()):
-            print(key, ":", d[key])
+                
+                
+        # d = sorted(d.items(), key=lambda x: x[1], reverse=True)
         
-        # lines = file.readlines() 
-        # print(type(lines))
+        
+        # for key in list(d.keys()):
+        #     print(key, ":", d[key])
+                
+        
+                
 
         # text_string = file.read()
         # print(text_string)
